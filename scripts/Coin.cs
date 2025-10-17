59ad960aee969eb1a78ac914d009419990266ce1
AntND <antonlenz508@gmail.com>
2025-10-17 14:32:28 +0200

    Coin

---
+++ scripts/Coin.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Coin : Area2D
+{
+
+    public override void _Ready()
+    {
+		BodyEntered += OnBodyEntered;
+    }
+
+	private void OnBodyEntered(Node2D body)
+	{
+		GD.Print("+1 Coin!");
+		QueueFree();
+	}
+
+
+}
